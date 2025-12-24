a76fa943cad7a84094a1aa9c3a94764d73675e7a
Filipe Demarque <filipe.demarque@gmail.com>
2025-12-24 18:28:58 -0300

    person generator in java

---
+++ src/main/java/com/kvervandi/persongenerator/PersonGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.kvervandi.persongenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonGeneratorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PersonGeneratorApplication.class, args);
+    }
+
+}
